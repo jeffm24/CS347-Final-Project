1e44841a4e128137113f1dbff108715c59ba4ed1
Nishanth Meni <nmeni@stevens.edu>
2015-04-23 21:43:11 -0400

    Added Group and Task Class

---
+++ Project/Task List/src/Group.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+
+public class Group {
+	private String name;
+	private int priority;
+	private String description;
+	ArrayList<Task> tasks;
+	
+	public Group(){
+		name = "";
+		priority = -1;
+		description = "";
+		tasks = new ArrayList<Task>();
+	}
+	
+	public Group(String n, int p, String d){
+		name = n;
+		priority = p;
+		description = d;
+		tasks = new ArrayList<Task>();
+	}
+	public int addTask(Task t){
+		if(t != null){
+			tasks.add(t);
+			return tasks.size();
+		}
+		return -1;
+	}
+	
+	public Task getTask(String name){
+		int i;
+		for(i = 0; i < tasks.size(); i++ )
+			if(tasks.get(i).getName().equalsIgnoreCase(name))
+				return tasks.get(i);
+		
+		return null;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPriority() {
+		return priority;
+	}
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
