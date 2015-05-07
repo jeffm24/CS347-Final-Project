0bc27fa141dbddb02d0e975563bc5b084098f5fd
Nishanth Meni <nmeni@stevens.edu>
2015-05-06 22:23:25 -0400

    quick fix

--- Project/Task List/src/TaskListTest.java
+++ Project/Task List/src/TaskListTest.java
@@ -1,17 +1,24 @@
 import static org.junit.Assert.*;
 
-import java.sql.Date;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import org.junit.Test;
 
 public class TaskListTest {
+	DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
 	ArrayList<Group> g = new ArrayList<Group>();
 	TaskList t = new TaskList(g);
-	Date d1 = new Date(1003);
-	Date d2 = new Date(1000);
-	Date a1 = new Date(2000);
-	Date a2 = new Date(3000);
+	@SuppressWarnings("deprecation")
+	Date d1 = new Date("1990/04/22");
+	@SuppressWarnings("deprecation")
+	Date a1 = new Date("1990/04/22");
+	@SuppressWarnings("deprecation")
+	Date d2 = new Date("1990/04/22");;
+	@SuppressWarnings("deprecation")
+	Date a2 = new Date("1990/04/22");
 	Sorting sorter = new Sorting();
 	Task t1 = new Task("Final Iteration", 2,
 			"Write Unit Test Cases for Project", d1, a1);
@@ -39,6 +46,20 @@ public class TaskListTest {
 		catch(Exception e){
 			
 		}
-		assertEquals(g,t.groups);
+		for(int i = 0; i < t.groups.size(); i++){
+			assertEquals(t.groups.get(i).getName(),g.get(i).getName());
+			assertEquals(t.groups.get(i).getDescription(),g.get(i).getDescription());
+			assertEquals(t.groups.get(i).getPriority(),g.get(i).getPriority());
+			for(int j = 0; j < t.groups.get(i).tasks.size(); j++)
+			{
+				assertEquals(t.groups.get(i).tasks.get(j).getName(),g.get(i).tasks.get(j).getName());
+				assertEquals(t.groups.get(i).tasks.get(j).getDescription(),g.get(i).tasks.get(j).getDescription());
+				assertEquals(t.groups.get(i).tasks.get(j).getPriority(),g.get(i).tasks.get(j).getPriority());
+				assertEquals(t.groups.get(i).tasks.get(j).getDueDate(),g.get(i).tasks.get(j).getDueDate());
+				assertEquals(t.groups.get(i).tasks.get(j).getAlarmDate(),g.get(i).tasks.get(j).getAlarmDate());
+			}
+		}
+	
+		
 	}
 }
